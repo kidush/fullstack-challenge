dd1135ee4af14d216550096f02f344b1d3dd7bd5
Thiago F. Lins <thiago.evandson@gmail.com>
2018-02-22 11:55:06 -0300

    Fix comic upvote in comics list

--- app/models/comic_vote.rb
+++ app/models/comic_vote.rb
@@ -1,7 +1,7 @@
 class ComicVote < ApplicationRecord
   def self.upvote(comic_id:)
     find_or_initialize_by(comic_id: comic_id).tap do |votes|
-      votes.votes += 1
+      votes.increment!(:votes)
     end
   end
 end
